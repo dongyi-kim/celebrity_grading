cd58053863eed2146c09d92d1db68cda448ff7d7
waps12b <waps12b@naver.com>
2016-11-26 13:58:04 +0900

    create word_collector project

---
+++ word_collector/index.js
@@ -0,0 +1,22 @@
+
+
+const MySQL = require('mysql');
+
+var connection = MySQL.createConnection({
+    host    :'codingmonster.net',
+    port : 3306,
+    user : 'datamining',
+    password : 'dm2016',
+    database:'celebrity_grading'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }else{
+        console.log('done');
+    }
+});
+
