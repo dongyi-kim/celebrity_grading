05bb6c4157076adb7cb19a7741a2e5d8d27b458c
tjdudwlsdl <edgard@ajou.ac.kr>
2016-11-26 19:11:21 +0900

    Update users.js

--- website/routes/users.js
+++ website/routes/users.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
+
+var connection = mysql.createConnection({
+    host    :'codingmonster.net',
+    port : 3306,
+    user : 'datamining',
+    password : 'dm2016',
+    database:'celebrity_grading'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }else{
+        console.log('done');
+    }
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -18,10 +37,16 @@ router.post('/', function (req, res) {
   else
     selection='useless';
 
-  console.log(req.body.selection);
+  console.log(req.body.word);
+    var word = req.body.word;
   console.log(selection);
 
-  res.send('respond with a resource');
+   connection.query('update words set '+selection+'='+selection+'+1 where (word=\''+word+'\')', function (err, result) {
+       console.log(result);
+    });
+
+
+    res.render('result', { title: 'Express'});
 });
 
 module.exports = router;
