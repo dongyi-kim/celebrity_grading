07a8741fb4c75737591d302c7566d5010cb7c3f8
clacis91@gmail.com <clacis91@gmail.com>
2016-11-27 14:21:58 +0900

    Bug fix

--- nlp/main.py
+++ nlp/main.py
@@ -22,30 +22,39 @@ curs = conn.cursor()
 #curs.execute("set names utf8")
 
 dir = "../word_collector/data"
-for name in os.listdir(dir):
-    path = os.path.join(dir, name)
+for filename in os.listdir(dir):
+    path = os.path.join(dir, filename)
+    name = filename.split(".")
+    name = unicode(name[0], 'utf-8')
+    print ("***" + name + "***")
 
     nlp_result = nlp.doNLP(path)
 
     up_word = 0
     new_word = 0
-    for word in nlp_result :
-        is_exist_query = "select * from words where word = \'" + word + "\';"
-        curs.execute(is_exist_query.encode('utf8'))
-        rows = curs.fetchall()
-        query = ""
 
-        if len(rows) > 0 :
+    remove_dup_list = list(set(nlp_result))
+    for word in remove_dup_list :
+        is_exist_query = "select count(*) from words where word = \'" + word + "\'"
+        curs.execute(is_exist_query.encode('utf8'))
+        (number_of_rows,) = curs.fetchone()
+        words_db_query = ""
+
+        if number_of_rows > 0 :
             up_word = up_word + 1
-            query = "update words set mentioned = mentioned + 1 where (word = \'" + word + "\')"
+            words_db_query = "update words set mentioned = mentioned + 1 where (word = \'" + word + "\')"
         else :
             new_word = new_word + 1
-            query = "insert into words(word, positive, negative, useless, mentioned) values(\'" + word + "\', 0, 0, 0, 1)"
+            words_db_query = "insert into words(word, positive, negative, useless, mentioned) values(\'" + word + "\', 0, 0, 0, 1)"
 
-        curs.execute(query.encode('utf8'))
+        curs.execute(words_db_query.encode('utf8'))
+
+        frequency_db_query = "insert into frequency values(\"" + name + "\", \"" + word + "\", " + str(nlp_result.count(word)) + ")"
+        curs.execute(frequency_db_query.encode('utf8'))
 
     conn.commit()
-    conn.close()
 
     print ("Updated words # : " + str(up_word))
     print ("Inserted words # : " + str(new_word))
+
+conn.close()
